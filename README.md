2b6782f374fad859e21acae67d06ace457e3de97
Antonella Del Vecchio <antodelvecchio9@gmail.com>
2022-01-25 16:34:05 -0300

    Readme added and comments deleted

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notas
+
+Este repositorio contiente varios customHooks para ayudarme a mí y a quien le sirva.
+
+🙂
--- useCounter.js
+++ useCounter.js
@@ -23,30 +23,3 @@ export const useCounter = ( initialState = 10 ) => {
         reset
     };
 }
-
-
-/*
-export const useCounter = ( initialState = 10 ) => {
-
-    const [state, setstate] = useState(initialState);
-
-    const increment = ( factor = 1 ) => {
-        setstate( state + factor );
-    };
-
-    const decrement = ( factor = 1 ) => {
-        setstate( state - factor );
-    };
-
-    const reset = () => {
-        setstate( initialState );
-    }
-
-    return {
-        state,
-        increment,
-        decrement,
-        reset
-    };
-}
-*/
